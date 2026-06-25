fddea9dc605aabcb81637fffaa349f59373aee94
Gomjong <kjo19990606@gmail.com>
2026-06-25 21:23:54 +0900

    feat: persist comments with PostgreSQL and Docker

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "hello",
+  host: "localhost",
+  database: "hello_comment",
+  password: "hello1234",
+  port: 5432,
+});
+
+
 const express = require("express");
 
 const app = express();
@@ -6,17 +17,25 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public")); // 브라우저가 요청한 파일이 public 폴더 안에 있으면 그대로 보내주라는 뜻
 
-const comments = [];
 
-app.post("/comment", (req, res) => {
-  comments.push(req.body);
+app.post("/comment", async (req, res) => {
 
-  res.redirect("/");
+    await pool.query(
+        "INSERT INTO comments (comment) VALUES ($1)",
+        [req.body.comment]
+    );
+
+    res.redirect("/");
 });
 
-app.get("/comments", (req, res) => {
-  res.send(comments);
-});
+app.get("/comments", async (req, res) => {
+
+    const result = await pool.query(
+        "SELECT * FROM comments"
+    );
+
+    res.send(result.rows);
+  });
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
